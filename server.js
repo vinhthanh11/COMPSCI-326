84c7d9f2db8119a0ff3ea205cced8101b64d98b6
Dang Nguyen <65774904+dangprolegend@users.noreply.github.com>
2024-05-14 01:52:58 -0400

    fix

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+import PouchDB from "pouchdb";
+
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const db = new PouchDB('dmteam');
 
 // Serve static files from the 'client' directory
 app.use(express.static(path.join(__dirname, 'src')));
 
 // Fallback to 'index.html' for any other request
+async function addDocument(doc) {
+  try {
+    const response = await db.put(doc);
+    console.log("Document created successfully", response);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
 app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'client/pages/home.html'));
 });
@@ -30,3 +44,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
